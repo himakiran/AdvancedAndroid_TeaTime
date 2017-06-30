761354aa606c4d84e0406a67261b4f8ad597b6a1
himakiran <himakiran@gmail.com>
2017-07-01 01:15:31 +0530

    Testing for intents with intent stubs

--- app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
@@ -16,24 +16,60 @@
 
 package com.example.android.teatime;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import android.app.Activity;
+
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.IsNot.not;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+
+
 // TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(JUnit4.class)
 public class OrderSummaryActivityTest {
 
+    private static final String emailMessage = "I just ordered a delicious tea from TeaTime.";
+
     // TODO (2) Add the rule that indicates we want to use Espresso-Intents APIs in functional UI tests
+    @Rule
+    public IntentsTestRule<OrderSummaryActivity> mActivityRule = new IntentsTestRule<>(OrderSummaryActivity.class);
 
 
     // TODO (3) Finish this method which runs before each test and will stub all external
     // intents so all external intents will be blocked
-
+    @Before
     public void stubAllExternalIntents() {
-
+        // By default Espresso Intents does not stub any Intents. Stubbing needs to be setup before
+        // every test run. In this case all external Intents will be blocked.
+        intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
     }
+
+
 
 
     // TODO (4) Finish this method which verifies that the intent sent by clicking the send email
     // button matches the intent sent by the application
 
     public void clickSendEmailButton_SendsEmail() {
+
+        onView(withId(R.id.send_email_button)).perform(click());
+        intended(allOf(hasAction(Intent.ACTION_SENDTO),hasExtra(Intent.EXTRA_TEXT, emailMessage)));
+
 
     }
 }
